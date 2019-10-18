128d867e1d42d5bfb60fba8225dc6954658b5739
Joshua Mills <joshua.mills@tufts.edu>
2019-10-18 10:17:10 -0400

    Fixed print statements, brought test_jmills.py up to new code functionality

--- lardly/data/larlite_track.py
+++ lardly/data/larlite_track.py
@@ -17,7 +17,7 @@ def visualize_larlite_track( larlite_track, track_id=None ):
             name = "tid[{}]".format(track_id)
         elif type(track_id) is str:
             name = track_id
-        
+
     track = {
         "type":"scatter3d",
         "x": xyz[:,0],
@@ -29,3 +29,41 @@ def visualize_larlite_track( larlite_track, track_id=None ):
     }
 
     return track
+
+def visualize_larlite_track_vtx( larlite_track ):
+
+    xyz = np.zeros( (1,3 ) )
+    xyz[0,0] = larlite_track.Vertex().x()
+    xyz[0,1] = larlite_track.Vertex().y()
+    xyz[0,2] = larlite_track.Vertex().z()
+    vtx = {
+        "type":"scatter3d",
+        "x": xyz[:,0],
+        "y": xyz[:,1],
+        "z": xyz[:,2],
+        'mode': 'markers',
+        'marker': {'size': 4},
+        "name":"",
+        "line":{"color":"rgb(0,255,0)","width":2},
+    }
+
+    return vtx
+
+def visualize_larlite_track_end( larlite_track ):
+
+    xyz = np.zeros( (1,3 ) )
+    xyz[0,0] = larlite_track.End().x()
+    xyz[0,1] = larlite_track.End().y()
+    xyz[0,2] = larlite_track.End().z()
+    End = {
+        "type":"scatter3d",
+        "x": xyz[:,0],
+        "y": xyz[:,1],
+        "z": xyz[:,2],
+        'mode': 'markers',
+        'marker': {'size': 4},
+        "name":"",
+        "line":{"color":"rgb(255,255,0)","width":2},
+    }
+
+    return End

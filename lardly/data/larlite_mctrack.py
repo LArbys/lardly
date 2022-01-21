e1fd9a2d4479c862082b3f5bdb93101f63231263
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2022-01-21 16:16:45 -0500

    clean up true trajectory overlays

--- lardly/data/larlite_mctrack.py
+++ lardly/data/larlite_mctrack.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+default_pid_colors = {2212:'rgb(153,55,255)', # protons
+                      13:'rgb(255,0,0)', # muons
+                      -13:'rgb(255,0,0)', # muons
+                      211:'rgb(255,128,255)',# pions
+                      -211:'rgb(255,128,255)',# pions
+                      0:'rgb(0,0,0)'# other
+                      }
+
 def extract_mctrackpts( mctrack, sce=None ):
     from larlite import larlite, larutil
     cm_per_tick = larutil.LArProperties.GetME().DriftVelocity()*0.5
@@ -16,25 +24,36 @@ def extract_mctrackpts( mctrack, sce=None ):
         steps_np[istep,:] = (x,step.Y(),step.Z())
     return steps_np
 
-def visualize_larlite_event_mctrack( event_mctrack, origin=None ):
+def visualize_larlite_event_mctrack( event_mctrack, origin=None,
+                                     color_labels=default_pid_colors,
+                                     width=3, color_by_origin=False ):
 
     track_vis = []
     print ("number of mctracks: ",event_mctrack.size())
     for itrack in range(event_mctrack.size()):
         mctrack = event_mctrack.at(itrack)
         steps_np = extract_mctrackpts( mctrack )
-        if mctrack.PdgCode()==2112:
+        pid = mctrack.PdgCode()
+        if pid==2112:
+            # skip neutrons
             continue
 
         if origin is not None and origin!=mctrack.Origin():
             continue
 
         # cosmic origin
-        color = 'rgb(0,0,255)'
-        if mctrack.Origin()==1:
-            # neutrino pixels
-            color = 'rgb(0,255,255)'
+        if color_by_origin:
+            color = 'rgb(0,0,255)'
+            if mctrack.Origin()==1:
+                # neutrino pixels
+                color = 'rgb(0,255,255)'
+        else:
+            if pid in color_labels:
+                color = color_labels[pid]
+            else:
+                color = color_labels[0]
 
+        
 
         trackvis = {
             "type":"scatter3d",
@@ -42,8 +61,8 @@ def visualize_larlite_event_mctrack( event_mctrack, origin=None ):
             "y":steps_np[:,1],
             "z":steps_np[:,2],
             "mode":"lines",
-            "name":"pdg[%d]\nid[%d]"%(mctrack.PdgCode(),mctrack.TrackID()),
-            "line":{"color":color,"width":2},
+            "name":"pdg[%d]\nid[%d]"%(pid,mctrack.TrackID()),
+            "line":{"color":color,"width":width},
             }
         track_vis.append( trackvis )
     return track_vis
